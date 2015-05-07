46791ba93f6d5b7a37b3ef7866d202bc46fc1682
张旭 <zhangxu@wecash.net>
2015-05-07 18:17:57 +0800

    ..

--- API/tieba/Home/Controller/UserController.class.php
+++ API/tieba/Home/Controller/UserController.class.php
@@ -5,6 +5,7 @@ class UserController extends Controller {
     public function index(){
         
     }
+	//获取用户信息
 	public function getUserInfo(){
 		$userId = I('userId');
 		$user = M('User');
@@ -13,6 +14,57 @@ class UserController extends Controller {
 		$arr['errCode'] = 200;
 		$arr['msg'] = '查询成功';
 		$arr['data'] = $data[0];
+		echo json_encode($arr);
+	}
+	//登录
+	public function login(){
+		$username = I('username');
+		$password = I('password');
+		$user = M('User');
+		$condition['username'] = $username;
+		$condition['password'] = md5($password);
+		$data = $user->where($condition)->select();
+//		echo json_encode($data);
+		if($data){
+			$arr['errCode'] = 200;
+			$arr['msg'] = '查询成功';
+			$arr['data'] = array(
+				'id' => $data[0]['id'],
+				'userId' => $data[0]['userid'],
+				'nickname' => $data[0]['nickname']
+			);
+		}else{
+			$arr['errCode'] = 105;
+			$arr['msg'] = '用户名或密码错误';
+		}
+		echo json_encode($arr);
+	}
+	//注册
+	public function register(){
+		$username = I('username');
+		$password = I('password');
+		$nickname = I('nickname');
+		$regtime = time();
+		$userId = md5($username);
+		$user = M('User');
+		$data['username'] = $username;
+		$data['password'] = md5($password);
+		$data['nickname'] = $nickname;
+		$data['regtime'] = $regtime;
+		$data['userId'] = $userId;
+		$result = $user->add($data);
+//		echo json_encode($result);
+		if($result>0){
+			$arr = array(
+				'errCode'=>200,
+				'msg'=>'注册成功'
+			);
+		}else{
+			$arr = array(
+				'errCode'=>101,
+				'msg'=>'注册失败'
+			);
+		}
 		echo json_encode($arr);
 	}
 }
--- js/common.js
+++ js/common.js
@@ -1,9 +1,18 @@
 //全局配置，没有登录则跳到登录
-var userId = $.fn.cookie('userId');
-	if(!userId){ //如果没有这个用户的custId
-		location.href = "login.html";
-	}
 
+
+if (location.href.indexOf("index")==-1&&location.href.indexOf('login')==-1&&location.href.indexOf('register')) {//不是这几个页面需要判断登录
+	var userId = $.fn.cookie('userId');
+	var returnUrl = location.href;
+	if(!userId){ //如果没有这个用户的custId
+		location.href = "login.html?returnUrl="+returnUrl;
+	}
+}
 var host = 'http://localhost:8080/tieba/API/tieba/Home/';
 
 
+//获取get参数方法
+function getLocationVal(para){
+	var temp = location.search.split(para+"=")[1] || "";
+	return temp.indexOf("&")>=0 ? temp.split("&")[0] : temp;
+}

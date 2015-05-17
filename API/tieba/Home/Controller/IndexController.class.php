19d7cfbf4e9a4b905afdc87b3bc04d14a79856ac
zhangxu <zhangxu@wecash.net>
2015-05-17 21:01:25 +0800

    。。

--- API/tieba/Home/Controller/IndexController.class.php
+++ API/tieba/Home/Controller/IndexController.class.php
@@ -3,6 +3,12 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        
+        $a ='2222';
+		$b = array(
+			'aa'=>'111',
+			'bb'=>'333'
+		);
+		$this->assign('b',$b);
+		$this->display();
     }
 }

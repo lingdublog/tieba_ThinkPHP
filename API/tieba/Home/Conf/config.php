557dbf7a9021e6bbb719d1c13a4367d29e8b1183
zhangxu <zhangxu@wecash.net>
2015-09-10 18:40:07 +0800

    d

--- API/tieba/Home/Conf/config.php
+++ API/tieba/Home/Conf/config.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
-	'DB_TYPE'=>'mysql',
-	'DB_HOST'=>'localhost',
-	'DB_NAME'=>'lianxi',
-	'DB_USER'=>'root',
-	'DB_PWD'=>'root',
+    //'配置项'=>'配置值'
+    'DB_TYPE'=>'mysql',
+    'DB_HOST'=>'qdm112364349.my3w.com',
+    'DB_NAME'=>'qdm112364349_db',
+    'DB_USER'=>'qdm112364349',
+    'DB_PWD'=>'1314521b',
 );
